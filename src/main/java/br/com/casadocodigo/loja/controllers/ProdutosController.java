86ade9424b790f10bf6b0da6d210f8f04d3843ba
Jhony <jhony.v.m@hotmail.com>
2019-10-24 11:38:21 -0300

    ProdutosController e view form.jsp com post method

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -0,0 +1,16 @@
+package br.com.casadocodigo.loja.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.casadocodigo.loja.models.Produto;
+
+@Controller
+public class ProdutosController {
+
+	@RequestMapping("/produtos")
+	public String gravar(Produto produto){
+	    System.out.println(produto);
+	    return "produtos/ok";
+	}
+}
