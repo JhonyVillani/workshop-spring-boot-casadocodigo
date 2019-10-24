56096600a1b8b840948fab99e2053d58f31b7884
Jhony <jhony.v.m@hotmail.com>
2019-10-24 17:35:24 -0300

    URL amigavel e detalhe.jsp

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,48 +25,56 @@ import br.com.casadocodigo.loja.validation.ProdutoValidation;
 @Controller
 @RequestMapping("/produtos")
 public class ProdutosController {
-
-    @Autowired
-    private ProdutoDAO produtoDao;
-    
-    @Autowired
+	
+	@Autowired
+	private ProdutoDAO dao;
+	
+	@Autowired
     private FileSaver fileSaver;
-    
-    @RequestMapping("/form")
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
+	}
+
+	@RequestMapping("/form")
 	public ModelAndView form(Produto produto) {
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
 		return modelAndView;
 	}
-    
-    @RequestMapping(method=RequestMethod.POST)
-    public ModelAndView gravar(MultipartFile sumario, @Valid Produto produto, 
-            BindingResult result, RedirectAttributes redirectAttributes) {
-
-        if(result.hasErrors()) {
-            return form(produto);
-        }
-
-        String path = fileSaver.write("arquivos-sumario", sumario);
-        produto.setSumarioPath(path);
-
-        produtoDao.gravar(produto);
-
-        redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
-
-        return new ModelAndView("redirect:produtos");
-    }
-    
-    @RequestMapping(method=RequestMethod.GET)
-    public ModelAndView listar(){
-        List<Produto> produtos = produtoDao.listar();
-        ModelAndView modelAndView = new ModelAndView("/produtos/lista");
-        modelAndView.addObject("produtos", produtos);
-        return modelAndView;
-    }
-    
-    @InitBinder
-    public void initBinder(WebDataBinder binder){
-        binder.addValidators(new ProdutoValidation());
-    }
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView gravar(MultipartFile sumario, @Valid Produto produto, BindingResult result, 
+				RedirectAttributes redirectAttributes){
+		
+		if(result.hasErrors()) {
+			return form(produto);
+		}
+		
+		String path = fileSaver.write("arquivos-sumario", sumario);
+		produto.setSumarioPath(path);
+		
+		dao.gravar(produto);
+		
+		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
+		
+		return new ModelAndView("redirect:produtos");
+	}
+	
+	@RequestMapping( method=RequestMethod.GET)
+	public ModelAndView listar() {
+		List<Produto> produtos = dao.listar();
+		ModelAndView modelAndView = new ModelAndView("produtos/lista");
+		modelAndView.addObject("produtos", produtos);
+		return modelAndView;
+	}
+	
+	@RequestMapping("/detalhe/{id}")
+	public ModelAndView detalhe(@PathVariable("id") Integer id){
+	    ModelAndView modelAndView = new ModelAndView("/produtos/detalhe");
+	    Produto produto = dao.find(id);
+	    modelAndView.addObject("produto", produto);
+	    return modelAndView;
+	}
 }
--- src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
+++ src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -14,14 +14,19 @@ import br.com.casadocodigo.loja.models.Produto;
 @Transactional
 public class ProdutoDAO {
 
-    @PersistenceContext
-    private EntityManager manager;
+	@PersistenceContext
+	private EntityManager manager;
 
-    public void gravar(Produto produto){
-        manager.persist(produto);
-    }
-    
-    public List<Produto> listar(){
-        return manager.createQuery("select p from Produto p", Produto.class).getResultList();
-    }
+	public void gravar(Produto produto) {
+		manager.persist(produto);
+	}
+
+	public List<Produto> listar() {
+		return manager.createQuery("select p from Produto p", Produto.class).getResultList();
+	}
+
+	public Produto find(Integer id) {
+		return manager.createQuery("select distinct(p) from Produto p join fetch p.precos precos where p.id = :id",
+				Produto.class).setParameter("id", id).getSingleResult();
+	}
 }
