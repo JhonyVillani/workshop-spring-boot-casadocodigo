940eebc42e5f87802fb5bebc92c8bf5dd2f055ad
Jhony <jhony.v.m@hotmail.com>
2019-10-24 18:07:08 -0300

    CarrinhoComprasController, entidades CarrinhoCompras e CarrinhoItem

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -16,18 +16,19 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import br.com.casadocodigo.loja.controllers.HomeController;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.infra.FileSaver;
+import br.com.casadocodigo.loja.models.CarrinhoCompras;
 
 @EnableWebMvc
-@ComponentScan(basePackageClasses = {HomeController.class, ProdutoDAO.class, FileSaver.class})
+@ComponentScan(basePackageClasses = {HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class})
 public class AppWebConfiguration {
 	
 	@Bean
-	public InternalResourceViewResolver internalResourceViewResolver() {
-		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-		internalResourceViewResolver.setPrefix( "/WEB-INF/views/" );
-		internalResourceViewResolver.setSuffix( ".jsp" );
-		
-		return internalResourceViewResolver;
+	public InternalResourceViewResolver internalResourceViewResolver(){
+	    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	    resolver.setPrefix("/WEB-INF/views/");
+	    resolver.setSuffix(".jsp");
+	    resolver.setExposedContextBeanNames("carrinhoCompras");
+	    return resolver;
 	}
 	
 	@Bean
