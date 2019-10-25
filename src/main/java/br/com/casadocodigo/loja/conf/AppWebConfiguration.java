7ab749e6e562ebfb8163a4dde9827987b496efcb
Jhony <jhony.v.m@hotmail.com>
2019-10-25 13:18:44 -0300

    Melhorando o home.jsp e habilitando cache

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -23,6 +26,7 @@ import br.com.casadocodigo.loja.models.CarrinhoCompras;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses= {HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class})
+@EnableCaching
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	
 	@Bean
@@ -31,7 +35,6 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix( ".jsp");
 		
-		//resolver.setExposeContextBeansAsAttributes(true);
 		resolver.setExposedContextBeanNames("carrinhoCompras");
 		
 		return resolver;
@@ -66,6 +69,11 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
+	
+	@Bean
+    public CacheManager cacheManager() {
+    	return new ConcurrentMapCacheManager();
+    }
     
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
