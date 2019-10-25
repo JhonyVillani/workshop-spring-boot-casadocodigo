54dde1733f64d66e1237f3d7775318fbc0e79160
Jhony <jhony.v.m@hotmail.com>
2019-10-25 11:15:18 -0300

    Enviando valor para a operadora e finalizacao assincrona

--- src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
+++ src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -7,21 +7,23 @@ import javax.servlet.ServletRegistration.Dynamic;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
-	
+public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[0];
+		return null;
 	}
-	
+
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-	    return new Class[] {AppWebConfiguration.class, JPAConfiguration.class};
+		return new Class[] {AppWebConfiguration.class, JPAConfiguration.class};
 	}
-	
+
+	@Override
 	protected String[] getServletMappings() {
-		return new String[]{"/"};
+		return new String[] {"/"};
 	}
-	
+
 	@Override
     protected Filter[] getServletFilters() {
         CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
