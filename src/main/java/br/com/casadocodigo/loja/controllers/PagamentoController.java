fc29373f460be8bf369173e3d63f72aee47d72ae
Jhony <jhony.v.m@hotmail.com>
2019-11-01 15:44:10 -0300

    Enviando email ao finalizar compra

--- src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
@@ -4,6 +4,9 @@ import java.util.concurrent.Callable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +18,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
 import br.com.casadocodigo.loja.models.DadosPagamento;
+import br.com.casadocodigo.loja.models.Usuario;
 
 @Controller
 @RequestMapping("/pagamento")
@@ -25,17 +29,22 @@ public class PagamentoController {
 	private CarrinhoCompras carrinho;
 	
 	@Autowired
-	RestTemplate restTemplate;
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private MailSender sender;
 	
 	@RequestMapping(value="/finalizar", method=RequestMethod.POST)
-	public Callable<ModelAndView>  finalizar(RedirectAttributes model){
+	public Callable<ModelAndView>  finalizar(@AuthenticationPrincipal Usuario usuario,
+			RedirectAttributes model){
 		return () -> {
 			String uri = "http://book-payment.herokuapp.com/payment";
 			
 			try {
 				String response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);
-				model.addFlashAttribute("sucesso", response);
 				System.out.println(response);
+				enviaEmailCompraProduto(usuario);
+				model.addFlashAttribute("sucesso", response);
 				return new ModelAndView("redirect:/produtos");
 			} catch (HttpClientErrorException e) {
 				e.printStackTrace();
@@ -43,5 +52,17 @@ public class PagamentoController {
 				return new ModelAndView("redirect:/produtos");
 			}
 		};
+	}
+
+	private void enviaEmailCompraProduto(Usuario usuario) {
+		SimpleMailMessage email = new SimpleMailMessage();
+		email.setSubject("Compra finalizada com sucesso");
+//		email.setTo(usuario.getEmail());
+		email.setTo("jhony.villani@dealerhonda.com.br");
+		email.setText("Compra aprovada com sucesso no valor de " + carrinho.getTotal());
+		email.setFrom("compras@casadocodigo.com.br");
+		
+		sender.send(email);
+		
 	}
 }
